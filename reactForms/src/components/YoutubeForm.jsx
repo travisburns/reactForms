1b0e60320e02e8beef50f5f4a70f4544b6f06b8a
Travis <travisjb0@gmail.com>
2023-08-02 21:54:10 -0700

    Learned and setup Managing form state basics

--- reactForms/src/components/YoutubeForm.jsx
+++ reactForms/src/components/YoutubeForm.jsx
@@ -43,19 +43,24 @@ import {useForm } from 'react-hook-form'
 
 function YoutubeForm() {
   const form = useForm()
+  const { register } = form
   
   
   return (
     <div className='table-auto'>
       <form className=''>
         <label htmlFor='username'>Username</label>
-        <input type="text" id="username" name="username" />
+        <input 
+        type="text" 
+        id="username" 
+       {...register("username")}
+       />
 
         <label htmlFor='email'>E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor='channel'>Channel</label>
-        <input type="text" id="Channel" name="Channel" />
+        <input type="text" id="Channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>
