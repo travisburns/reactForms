91a1546766e94ef70843e00bf4afae9552b2bc6d
Travis <travisjb0@gmail.com>
2023-08-05 22:58:15 -0700

    Added basics of react hook forms in order to simplify forms in react

--- reactForms/src/App.jsx
+++ reactForms/src/App.jsx
@@ -3,15 +3,49 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import YoutubeForm from './components/YoutubeForm'
+import {useForm} from 'react-hook-form'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+
+
+  const { register, handleSubmit } = useForm()
+  
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+
 
   return (
-   
-     <YoutubeForm />
-    
-  )
+    <form className="App" onSubmit={handleSubmit(onSubmit)}>
+      <h1>Sign Up</h1>
+      <label>First Name:</label>
+      <input {...register("firstName")} id="firstName" />
+
+      <label>Last Name:</label>
+      <input {...register("firstName")}  />
+
+      <label>Gender</label>
+      <select {...register("gender")}>
+        <option>Select...</option>
+        <option>Male</option>
+        <option>Female</option>
+      </select>
+
+      <label>Username</label>
+      <input {...register("username")} />
+
+      <label>Email</label>
+      <input {...register("email")}  />
+
+      <label>About you</label>
+      <textarea  />
+
+      <input type="submit" />
+    </form>
+  );
+ 
 }
 
 export default App
