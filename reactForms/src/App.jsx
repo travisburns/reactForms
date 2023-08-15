380fb3dffc5a254ecd210b83a108b4bb683cb607
Travis <travisjb0@gmail.com>
2023-08-15 13:17:50 -0700

    practiced forms setup lesson -2

--- reactForms/src/App.jsx
+++ reactForms/src/App.jsx
@@ -1,51 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import YoutubeForm from './components/YoutubeForm'
-import {useForm} from 'react-hook-form'
+import React, {useState} from 'react'
+import { YoutubeForm } from './components/YoutubeForm'
 
 function App() {
- 
-
-
-  const { register, handleSubmit } = useForm()
+  const [count, setCount] = useState()
   
-  const onSubmit = (data) => {
-    console.log(data)
-  }
-
-
-
   return (
-    <form className="App" onSubmit={handleSubmit(onSubmit)}>
-      <h1>Sign Up</h1>
-      <label>First Name:</label>
-      <input {...register("firstName")} id="firstName" />
-
-      <label>Last Name:</label>
-      <input {...register("firstName")}  />
-
-      <label>Gender</label>
-      <select {...register("gender")}>
-        <option>Select...</option>
-        <option>Male</option>
-        <option>Female</option>
-      </select>
-
-      <label>Username</label>
-      <input {...register("username")} />
-
-      <label>Email</label>
-      <input {...register("email")}  />
-
-      <label>About you</label>
-      <textarea  />
-
-      <input type="submit" />
-    </form>
-  );
- 
+    <div>
+      <YoutubeForm />
+    </div>
+  )
 }
 
 export default App
--- reactForms/src/components/YoutubeForm.tsx
+++ reactForms/src/components/YoutubeForm.tsx
@@ -1,87 +1,31 @@
-// import React from 'react'
-// import { useForm } from 'react-hook-form'
-// import { DevTool } from '@hookform/devtools'
-
-// let renderCount = 0 
-
-
-// const YoutubeForm = () => {
-
-//   const form = useForm();
-//   const {register, control } = form
- 
-
-//  renderCount++
-//   return (
-    
-//     <div className='form'>
-//       <h1>YouTube Form({renderCount/2}) </h1>
-//       <form>
-//         <label htmlFor='username'>Username</label>
-//         <input type="text" id="username" {...register("username")}/>
-
-//         <label htmlFor='email'>E-mail</label>
-//         <input type="email" id="email" {...register("email")}/>
-
-//         <label htmlFor='username'>Username</label>
-//         <input type="text" id="channel" {...register("channel")}/>
-        
-//         <button>Submit</button>
-//       </form>
-//       <DevTool control={control}/>
-//     </div>
-//   )
-// }
-
-// export default YoutubeForm
-
-
-
-//hook form 2 -form setup: create the functional component, create a corm within the div, create a label with username text, username, repeate the rest but change values 
 import React from 'react'
-import {useForm } from 'react-hook-form'
-import { DevTool } from "@hookform/devtools"
 
-let renderCount = 0;
 
-type FormValues = {
-  username: string
-  email: string 
-  channel: string 
-}
 
-function YoutubeForm() {
-  const form = useForm<FormValues>()
-  const { register, control, handleSubmit } = form
+
+
+
+
+
+export const YoutubeForm = () => {
   
-  const onSubmit = (data: FormValues) => {
-    console.log('Form Submit', data)
-  }
-
-  renderCount++
-
   return (
     <div>
-      <h1>Youtube Form ({renderCount / 2})</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className=''>
+      <form>
         <label htmlFor='username'>Username</label>
-        <input 
-        type="text" 
-        id="username" 
-       {...register("username")}
-       />
+        <input type='text' id="username" name="username" />
 
-        <label htmlFor='email'>E-mail</label>
-        <input type="email" id="email" {...register("email")} />
+        <label htmlFor='email'>Email</label>
+        <input type='email' id="email" name="email" />
 
         <label htmlFor='channel'>Channel</label>
-        <input type="text" id="Channel" {...register("channel")} />
+        <input type='text' id="channel" name="channel" />
 
-        <button>Submit</button>
+        <button onClick={() => {}}>Submit</button>
+
       </form>
-      <DevTool  control={control}/>
     </div>
   )
 }
 
-export default YoutubeForm
+
